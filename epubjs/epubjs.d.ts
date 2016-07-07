cedd930ee49b1a2e6302ce641336a92eaafe0dec
Victor Milovanov <lostfreeman@gmail.com>
2016-07-06 23:48:21 -0700

    initial epubjs typing

---
+++ epubjs/epubjs.d.ts
@@ -0,0 +1,32 @@
+﻿// Type definitions for EPUBJS
+// Project: https://github.com/futurepress/epub.js/
+// Definitions by: Victor Milovanov https://plus.google.com/+ВикторМилованов/
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+declare namespace EPubJS {
+    /** @summary */
+    interface IEPubBook {
+        /** @summary */
+        open(bookUrl: string): void;
+        open(epubBlob: ArrayBuffer): void;
+        renderTo(element: HTMLElement): void;
+    }
+
+    /** @summary */
+    interface OpenBookOptions {
+        bookPath?: string;
+        version?: number;
+        restore?: boolean;
+        storage?: boolean;
+        spreads?: boolean;
+        fixedLayout?: boolean;
+        width?: boolean;
+        height?: boolean;
+    }
+}
+
+/** @summary opens EPub book from the specified URL, and default options */
+declare function ePub(bookUrl: string): EPubJS.IEPubBook;
+declare function ePub(epubBlob: ArrayBuffer): EPubJS.IEPubBook;
+declare function ePub(bookUrl: string, options: EPubJS.OpenBookOptions): EPubJS.IEPubBook;
+declare function ePub(options: EPubJS.OpenBookOptions): EPubJS.IEPubBook;
